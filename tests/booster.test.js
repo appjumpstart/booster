f722bcc7c9e556f029bc6e5a7dd3d168a973b2c7
Ian Walter <public@iankwalter.com>
2018-11-03 20:35:23 -0400

    Adding test skeletons

--- tests/booster.test.js
+++ tests/booster.test.js
@@ -1,3 +1,11 @@
-test('TODO', () => {
+test('booster applies migrations and seeds on the default database', () => {
+
+})
+
+test('booster applies migrations and seeds on the test database', () => {
+
+})
+
+test('booster applies migrations and seeds of specified knexfile', () => {
 
 })
