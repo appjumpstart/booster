7dc81e962768462dae8e60a06ee8e25d8ce01e0d
Ian Walter <public@iankwalter.com>
2018-11-05 20:49:09 -0500

    Fixing dirty snapshot issue

--- tests/fixtures/knexfile.js
+++ tests/fixtures/knexfile.js
@@ -1,8 +1,6 @@
-require('dotenv').config('../../.env')
-
-const database = process.env.DB_NAME
-const user = process.env.CI ? 'postgres' : process.env.DB_USER
-const password = process.env.CI ? '' : process.env.DB_PASS
+const database = 'booster'
+const user = 'postgres'
+const password = process.env.CI ? '' : 'williemayshayes'
 
 const development = { client: 'pg', connection: { database, user, password } }
 const connection = { database: `${database}_test`, user, password }
