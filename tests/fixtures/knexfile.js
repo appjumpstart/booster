c79c5b13d14eb33284bf8489353afc4c1a654dc8
Ian Walter <public@iankwalter.com>
2018-11-05 20:36:22 -0500

    Finishing tests

--- tests/fixtures/knexfile.js
+++ tests/fixtures/knexfile.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
+require('dotenv').config('../../.env')
 
 const database = process.env.DB_NAME
-const user = process.env.DB_USER
-const password = process.env.DB_PASS
+const user = process.env.CI ? 'postgres' : process.env.DB_USER
+const password = process.env.CI ? '' : process.env.DB_PASS
 
 const development = { client: 'pg', connection: { database, user, password } }
 const connection = { database: `${database}_test`, user, password }
